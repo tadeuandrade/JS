39af1b25624e00c620936583c64c1214e5a33d3b
tadeuandrade <tadeuandrade98@hotmail.com>
2021-06-16 21:47:12 -0300

    29

--- Exercicios_Fundamentos_Estrutura_de_controle_e_Funcoes/ex28.js
+++ Exercicios_Fundamentos_Estrutura_de_controle_e_Funcoes/ex28.js
@@ -1,11 +1,15 @@
 verificaint = (vet) => {
+    let countpar=0
+    let countimpar=0
+
     for (let index = 0; index < vet.length; index++) {
         if (vet[index] % 2 == 0) {
-            console.log(`${vet[index]} é par`)
+            countpar++
         } else{
-            console.log(`${vet[index]} é impar`)
+           countimpar++
         }
 
     }
+    console.log(`${countpar} numeros pares e ${countimpar} numeros impares`)
 }
 verificaint([1,2,3,4]);
---
+++ Exercicios_Fundamentos_Estrutura_de_controle_e_Funcoes/ex29.js
@@ -0,0 +1,14 @@
+intervalo = (vet) => {
+    let forainter = 0
+    let dentrointer = 0
+    for (let i = 0; i < vet.length; i++) {
+        if (vet[i] >= 10 && vet[i] <= 20) {
+            dentrointer++
+        } else {
+            forainter++
+        }
+    }
+    console.log(`${forainter} estao fora do intervalo e ${dentrointer} estao dentro do intervalo`)
+}
+
+intervalo([1,2,15,18,30,33,34])
