beedae18c5da91128b44c33e9cb361553914d200
Tadeu <tadeuandrade98@hotmail.com>
2022-08-17 19:45:59 -0300

    XMLHttpRequest #03

--- ajax/server.js
+++ ajax/server.js
@@ -6,8 +6,27 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, '.')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.')
+        }
+    })
+})
 
 //app.get('/teste', (req, res)=> res.send('ok'))
-app.listen(8080, ()=> console.log('Executando'))
+app.listen(8080, () => console.log('Executando'))
 
 
