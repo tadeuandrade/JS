1180521ea35de3542fea61abcee28ec0e3c5d258
Tadeu <tadeuandrade98@hotmail.com>
2022-08-21 16:14:31 -0300

    Ajax com jQuery

--- ajax/server.js
+++ ajax/server.js
@@ -26,6 +26,23 @@ app.post('/upload', (req, res) => {
     })
 })
 
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
+app.get('/parOuImpar', (req, res) => {
+
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+
+})
+
+
 //app.get('/teste', (req, res)=> res.send('ok'))
 app.listen(8080, () => console.log('Executando'))
 
